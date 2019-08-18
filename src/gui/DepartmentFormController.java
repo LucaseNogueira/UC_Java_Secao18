4d1f3ccb55b45354aa42d2b898ef1a12a0b18706
LucaseNogueira <lucasenogueira@outlook.com>
2019-08-18 13:06:35 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -8,7 +8,9 @@ import gui.util.Utils;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,6 +19,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exception.ValidationException;
 import model.service.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -67,7 +70,11 @@ public class DepartmentFormController implements Initializable {
             service.saveOrUpdate(entity);
             notifyDataChangeListener();
             Utils.currentStage(event).close();
-        } catch (DbException e) {
+        } 
+        catch(ValidationException e){
+            setErrorMessages(e.getErrors());
+        }
+        catch (DbException e) {
             Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
         }
     }
@@ -98,8 +105,18 @@ public class DepartmentFormController implements Initializable {
     private Department getFormData() {
         Department obj = new Department();
 
+        ValidationException exception = new ValidationException("Validation error");
+        
         obj.setId(Utils.tryParseToInt(txtId.getText()));
+        
+        if(txtName.getText() == null || txtName.getText().trim().equals("")){
+            exception.addError("name","Field can't be empty");
+        }
         obj.setName(txtName.getText());
+        
+        if(exception.getErrors().size() > 0){
+            throw exception;
+        }
 
         return obj;
     }
@@ -107,6 +124,14 @@ public class DepartmentFormController implements Initializable {
     private void notifyDataChangeListener() {
         for (DataChangeListener listener : dataChangeListeners) {
             listener.onDataChanged();
+        }
+    }
+    
+    private void setErrorMessages(Map<String,String> errors){
+        Set<String> fields = errors.keySet();
+        
+        if(fields.contains("name")){
+            labelErrorName.setText(errors.get("name"));
         }
     }
 }
